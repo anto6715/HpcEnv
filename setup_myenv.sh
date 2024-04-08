1c24fd5c661e73c2299b34c786402382e1dd0dcf
Antonio Mariani <antonio.mariani@cmcc.it>
2024-04-08 11:17:41 +0200

    add default load of bash_mamba

--- setup_myenv.sh
+++ setup_myenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TO_LOAD=("bash_settings" "bash_aliases" "bash_functions")
+TO_LOAD=("bash_settings" "bash_aliases" "bash_functions" "bash_mamba")
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 for to_load in "${TO_LOAD[@]}"; do
