9b49a10c915ccdfb2f5c6bf2e9ec9f74b980e7e0
Antonio Mariani <antonio.mariani@cmcc.it>
2026-02-28 14:25:44 +0100

    add more aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# Lazygit
+alias lg="lazygit"
+
 # Move between partitions
 if [ "${HPC_SYSTEM:-}" == "juno" ]; then
     alias myw='cd /work/cmcc/$USER'
     alias myd='cd /data/cmcc/$USER'
 fi
 
-# bash cmd
+# Bash cmd
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
@@ -23,7 +26,6 @@ fi
 # Kerberos
 alias kinit_otp='kinit -n;kinit -T FILE:/tmp/krb5cc_`id -u`'
 
-
 # Change accounts
 alias @="switch_user"
 
@@ -31,3 +33,26 @@ alias @="switch_user"
 alias @c="conda activate"
 alias @d="conda deactivate"
 
+# Git
+alias g='git'
+alias ga='git add'
+alias gf='git fetch'
+alias gs='git status'
+alias gss='git status -s'
+alias gl='git pull'
+alias gb='git branch '
+alias gbr='git branch -r'
+alias gd='git diff'
+alias gco='git checkout '
+alias gcob='git checkout -b '
+alias gre='git remote'
+alias gres='git remote show'
+alias glgg='git log --graph --max-count=5 --decorate --pretty="oneline"'
+alias gm='git merge'
+alias gp='git push'
+alias gpo='git push origin'
+alias ggpush='git push origin $(current_branch)'
+alias gc='git commit -v'
+alias gcm='git commit -m'
+alias gcmnv='git commit --no-verify -m'
+alias gcanenv='git commit --amend --no-edit --no-verify'
