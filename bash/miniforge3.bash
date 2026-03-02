d6137225b398f9e280a62152d63b682208be6259
Antonio Mariani <antonio.mariani@cmcc.it>
2026-03-02 12:17:10 +0100

    support to MAMBA_ROOT_PREFIX

--- bash/miniforge3.bash
+++ bash/miniforge3.bash
@@ -1,19 +1,19 @@
-__base_conda_path__="$HOME/.miniforge3"
+MAMBA_ROOT_PREFIX="$HOME/.miniforge3"
 
-__conda_setup="$('$__base_conda_path__/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('$MAMBA_ROOT_PREFIX/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "$__base_conda_path__/etc/profile.d/conda.sh" ]; then
-        . "$__base_conda_path__/etc/profile.d/conda.sh"
+    if [ -f "$MAMBA_ROOT_PREFIX/etc/profile.d/conda.sh" ]; then
+        . "$MAMBA_ROOT_PREFIX/etc/profile.d/conda.sh"
     else
-        export PATH="$__base_conda_path__/bin:$PATH"
+        export PATH="$MAMBA_ROOT_PREFIX/bin:$PATH"
     fi
 fi
 unset __conda_setup
 
-if [ -f "$__base_conda_path__/etc/profile.d/mamba.sh" ]; then
-    . "$__base_conda_path__/etc/profile.d/mamba.sh"
+if [ -f "$MAMBA_ROOT_PREFIX/etc/profile.d/mamba.sh" ]; then
+    . "$MAMBA_ROOT_PREFIX/etc/profile.d/mamba.sh"
 fi
 
 # <<< conda initialize <<<
